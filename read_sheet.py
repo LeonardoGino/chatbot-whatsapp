d5267eff278f3f056bde2df5828d103778171bdc
LeonardoGino <gleonardo@zynga.com>
2024-06-08 22:59:12 -0300

    set google sheet id

--- read_sheet.py
+++ read_sheet.py
@@ -4,7 +4,7 @@ from config import credentials  # Importar las credenciales desde config.py
 
 # Autenticación y acceso al Google Sheet
 gc = gspread.authorize(credentials)
-sheet = gc.open_by_key('your_google_sheet_id').sheet1  # Reemplaza 'your_google_sheet_id' con el ID real de tu Google Sheet
+sheet = gc.open_by_key('1TpYk7uNYdfkMK0yDO4QMCi_p05jm4o1h0IFysQ9hgSs').sheet1  # Reemplaza 'your_google_sheet_id' con el ID real de tu Google Sheet
 
 # Obtener los datos como un DataFrame de pandas
 data = sheet.get_all_records()
