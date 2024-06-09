161080bdeb4954d42df5133732e147536d4635c2
LeonardoGino <gleonardo@zynga.com>
2024-06-08 23:09:48 -0300

    refactor config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
     GOOGLE_SHEETS_CREDENTIALS = json.loads(os.getenv('GOOGLE_SHEETS_CREDENTIALS'))
 
-# Crear las credenciales de Google Sheets
-credentials_info = Config.GOOGLE_SHEETS_CREDENTIALS
-credentials = Credentials.from_service_account_info(credentials_info)
+# Cargar las credenciales desde las variables de entorno
+credentials_info = json.loads(os.getenv('GOOGLE_SHEETS_CREDENTIALS'))
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
